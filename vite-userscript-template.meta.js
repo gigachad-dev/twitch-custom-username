cacc5afc8014b769549cad10bc667deebd1e8706
crashmax-dev <crashmax.dev@gmail.com>
2023-06-07 12:06:54 +0000

    Deploying to gh-pages from @ gigachad-dev/twitch-custom-username@b73bd1a7d35ac34430db7a6df131a0206256c5b7 🚀

---
+++ vite-userscript-template.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name        vite-userscript-template
+// @version     0.0.2
+// @author      crashmax <userscript@crashmax.ru>
+// @license     MIT
+// @homepage    https://crashmax-dev.github.io/vite-userscript-template/
+// @match       http://localhost:3000
+// @match       https://example.com
+// @grant       GM_addStyle
+// @updateURL   https://crashmax-dev.github.io/vite-userscript-template/vite-userscript-template.meta.js
+// @downloadURL https://crashmax-dev.github.io/vite-userscript-template/vite-userscript-template.user.js
+// ==/UserScript==
