b0dca2d75bff7fdb712cd6181ae1c29a5b7bf360
crashmax-dev <crashmax.dev@gmail.com>
2023-06-14 08:21:21 +0000

    Deploying to gh-pages from @ gigachad-dev/twitch-custom-username@e42fec46feb074e5a1412b52941b536b7d487b8f 🚀

--- twitch-custom-username.meta.js
+++ twitch-custom-username.meta.js
@@ -1,10 +1,14 @@
 // ==UserScript==
 // @name        twitch-custom-username
-// @version     1.0.0
+// @version     1.1.0
 // @author      crashmax <userscript@crashmax.ru>
 // @license     MIT
 // @homepage    https://gigachad-dev.github.io/twitch-custom-username/
+// @resource    iframe-storage-url https://gigachad-dev.github.io/twitch-custom-username/index.html
 // @match       https://*.twitch.tv/*
+// @grant       GM_setValue
+// @grant       GM_getValue
+// @grant       GM_addValueChangeListener
 // @updateURL   https://gigachad-dev.github.io/twitch-custom-username/twitch-custom-username.meta.js
 // @downloadURL https://gigachad-dev.github.io/twitch-custom-username/twitch-custom-username.user.js
 // ==/UserScript==
